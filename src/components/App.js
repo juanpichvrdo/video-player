25374361b2c5d42ca70357ec244398003c9a70cc
Juan Daniel Pichardo <jpichardopena@gmail.com>
2019-02-21 00:36:51 -0400

    Create VideoList and VideoItem component and pass video state as props

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import SearchBar from "./SearchBar";
 import youtube from "../apis/youtube";
+import VideoList from "./VideoList";
 
 class App extends Component {
   state = { videos: [] };
@@ -17,8 +18,8 @@ class App extends Component {
   render() {
     return (
       <div className="ui container">
-        <SearchBar onFormSubmit={this.onTermSubmit} />I have{" "}
-        {this.state.videos.length}
+        <SearchBar onFormSubmit={this.onTermSubmit} />
+        <VideoList videos={this.state.videos} />
       </div>
     );
   }
