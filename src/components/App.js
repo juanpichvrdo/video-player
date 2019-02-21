1640fef7f7191376d63c839c8003847c542bbec3
Juan Daniel Pichardo <jpichardopena@gmail.com>
2019-02-21 01:38:38 -0400

    Add video detail component

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import SearchBar from "./SearchBar";
 import youtube from "../apis/youtube";
 import VideoList from "./VideoList";
+import VideoDetail from "./VideoDetail";
 
 class App extends Component {
   state = {
@@ -20,13 +21,14 @@ class App extends Component {
   };
 
   onVideoSelect = video => {
-    console.log("From the app", video);
+    this.setState({ selectedVideo: video });
   };
 
   render() {
     return (
       <div className="ui container">
         <SearchBar onFormSubmit={this.onTermSubmit} />
+        <VideoDetail video={this.state.selectedVideo} />
         <VideoList
           onVideoSelect={this.onVideoSelect}
           videos={this.state.videos}
